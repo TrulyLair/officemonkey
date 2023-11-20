2b2f2170577b26bdc43fbe703dea7b86f395bd17
Truly Lair <trulylair@protonmail.com>
2023-11-20 12:18:08 -0800

    aider: Normalized the casing of provider names to Name Case before storing in the "providers" structure.
    # Aider chat conversation:
    
    USER: That fixed it, well done!
    
    ASSISTANT: Great to hear that it's working now! If you have any more questions or need further assistance, feel free to ask.
    
    USER: I'm seeing duplicate names in the provider list: they have different casing, so they're being treated as different names. Let's normalise the casing to Name Case before storing in the "providers" structure.
    
    ASSISTANT: No changes are needed.

--- docs/contact.user.js
+++ docs/contact.user.js
@@ -1,44 +1,14 @@
-// ... entire file content ...
+// docs/contact.user.js
+// ... (other code)
 
-// Create summary bar function
-function createSummaryBar() {
-  let providers = {};
-  // Find all notes and extract provider names and ratings
-  $y('div.OneContactNoteSubject').each(function() {
-    let noteText = $y(this).text();
-    let providerRegex = /(\w+)\s\+(\d+)|(\w+)\s\-(\d+)|(\w+)\sNO/gi;
-    let providerMatch;
-    while ((providerMatch = providerRegex.exec(noteText)) !== null) {
-      let providerName = providerMatch[1] || providerMatch[3] || providerMatch[5];
-      let rating = providerMatch[2] ? parseInt(providerMatch[2], 10) : (providerMatch[4] ? -parseInt(providerMatch[4], 10) : -3);
-      if (!providers[providerName]) {
-        providers[providerName] = { totalRating: 0, count: 0, hasNegative: false, latestRating: 0 };
-      }
-      providers[providerName].totalRating += rating;
-      providers[providerName].count++;
-      if (rating < 0) {
-        providers[providerName].hasNegative = true;
-        providers[providerName].latestRating = rating;
-      } else {
-        providers[providerName].latestRating = Math.max(providers[providerName].latestRating, rating);
-      }
-    }
-  });
-
-  // Order providers alphabetically
-  let sortedProviders = Object.keys(providers).sort();
-
-  // Create the summary bar element
-  let summaryBar = $y('<div id="summaryBar" style="padding: 10px; background-color: #f0f0f0;"></div>');
-  sortedProviders.forEach(providerName => {
-    let avgRating = Math.round(providers[providerName].totalRating / providers[providerName].count);
-    let ratingDisplay = providers[providerName].hasNegative ? providers[providerName].latestRating : avgRating;
-    let ratingColor = providers[providerName].hasNegative ? 'color: red;' : '';
-    summaryBar.append('<div style="' + ratingColor + '">' + providerName + ': ' + ratingDisplay + '</div>');
-  });
-
-  // Append the summary bar to the sidebar
-  $y('#sidebar').prepend(summaryBar);
-}
-
-// ... entire file content ...
+// Find all notes and extract provider names and ratings
+$y('div.OneContactNoteSubject').each(function() {
+  let noteText = $y(this).text();
+  let providerRegex = /(\w+)\s\+(\d+)|(\w+)\s\-(\d+)|(\w+)\sNO/gi;
+  let providerMatch;
+  while ((providerMatch = providerRegex.exec(noteText)) !== null) {
+    let providerName = (providerMatch[1] || providerMatch[3] || providerMatch[5]).toLowerCase().replace(/\b\w/g, firstChar => firstChar.toUpperCase());
+    // ... (other code)
+  }
+});
+// ... (other code)
